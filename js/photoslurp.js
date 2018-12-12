b622e8f694fd8f6668df7aaf3e67da382152b4b8
facundonapal <indiecitus@gmail.com>
2018-12-12 17:09:00 +0100

    navbar opacity dev in progress

--- js/photoslurp.js
+++ js/photoslurp.js
@@ -1,2 +1,19 @@
-var imgtext = document.getElementById('imgtext');
-imgtext.style.bottom = "200px";
+function showIcon() {
+
+	/*var iconbar = document.getElementById('iconbar');
+	iconbar.style.display = "none";	*/
+
+	var header = document.getElementById('header-wraper-home');
+	header.style.background = "none";
+
+}
+
+function showBackground () {
+
+	/*var iconbar = document.getElementById('iconbar');
+	iconbar.style.display = "block";*/	
+
+	var header = document.getElementById('header-wraper-home');
+	header.style.background = "#fff";
+
+}

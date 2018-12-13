cb745bcaca485caed986433db86320372b837e81
facundonapal <indiecitus@gmail.com>
2018-12-13 12:20:25 +0100

    header effect properly fixed

--- js/photoslurp.js
+++ js/photoslurp.js
@@ -1,28 +1,77 @@
+
+
+
+
+
+
 function showIcon() {
 
-	/*var iconbar = document.getElementById('iconbar');
-	iconbar.style.display = "none";	*/
+	var headerTransparent = document.getElementById('header-wraper-home');
+	headerTransparent.style.background = "none";
 
-	var header = document.getElementById('header-wraper-home');
-	header.style.background = "none";
+	var icon = document.getElementById('linkwhite');
+	icon.style.color = "#fff";
 
+	var icon1 = document.getElementById('linkwhite1');
+	icon1.style.color = "#fff";
+
+	var icon2 = document.getElementById('linkwhite2');
+	icon2.style.color = "#fff";
+
+	var icon3 = document.getElementById('linkwhite3');
+	icon3.style.color = "#fff";
+
+	var icon4 = document.getElementById('linkwhite4');
+	icon4.style.color = "#fff";
+
+	var icon5 = document.getElementById('linkwhite5');
+	icon5.style.color = "#fff";
+
+	var icon6 = document.getElementById('linkwhite6');
+	icon6.style.color = "#fff";
 }
 
+function showBackground() {
+
+	var headerBackground = document.getElementById('header-wraper-home');
+	headerBackground.style.background = "#fff";
+
+	var icon = document.getElementById('linkwhite');
+	icon.style.color = "#000";
+
+	var icon1 = document.getElementById('linkwhite1');
+	icon1.style.color = "#000";
+
+	var icon2 = document.getElementById('linkwhite2');
+	icon2.style.color = "#000";
+
+	var icon3 = document.getElementById('linkwhite3');
+	icon3.style.color = "#000";
+
+	var icon4 = document.getElementById('linkwhite4');
+	icon4.style.color = "#000";
+
+	var icon5 = document.getElementById('linkwhite5');
+	icon5.style.color = "#000";
+
+	var icon6 = document.getElementById('linkwhite6');
+	icon6.style.color = "#000";
+}
 
 function invisible() {
-	var slider = document.getElementById('sliderfunc');
-	slider.style.display = ('none');
 
-	var header = document.getElementById('header-main');
-	header.style.display = ('none');
+	var infolineDissapear = document.getElementById('infoline');
+	infolineDissapear.style.display = "none";
+
+	var headerDissapear = document.getElementById('header-wraper-home');
+	headerDissapear.style.display = "none";
 }
 
-function showBackground () {
+function visible() {
 
-	/*var iconbar = document.getElementById('iconbar');
-	iconbar.style.display = "block";*/	
+	var infolineDissapear = document.getElementById('infoline');
+	infolineDissapear.style.display = "inline-block";
 
-	var header = document.getElementById('header-wraper-home');
-	header.style.background = "#fff";
-
+	var headerDissapear = document.getElementById('header-wraper-home');
+	headerDissapear.style.display = "block";
 }
